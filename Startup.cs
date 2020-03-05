b00b5b6e3c127295c252b90da03008a751dca4ce
Shrikrishna Khose <krishna.khose@gmail.com>
2020-03-04 23:19:13 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ namespace TodoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
